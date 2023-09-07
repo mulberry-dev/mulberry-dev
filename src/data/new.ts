ec482aa43258787cd89be9f27365cadd5f2f7286
Santiago Morera <santi.iztli@gmail.com>
2023-09-06 21:20:54 -0600

    update

---
+++ src/data/new.ts
@@ -0,0 +1,147 @@
+export const portfolio = [
+    {
+      id: 0,
+      name: "eCommerce",
+      thumbnail: "/images/thumbnails/Thumbnail-e-Commerce-Backend.webp",
+      preview: "/images/Webp/eCommerce.webp",
+      url: "https://ecommerce-backend-43495.fly.dev/",
+      tech: [
+        "/images/Icons/tech/node.webp",
+        "https://handlebarsjs.com/images/handlebars_logo.png",
+        "/images/Icons/tech/mocha.svg",
+        "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
+        "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png?20141107110902",
+        "https://www.pngall.com/wp-content/uploads/13/Mongodb-PNG-Image-HD.png",
+        "https://sass-lang.com/assets/img/logos/logo.svg",
+        "https://static.cdnlogo.com/logos/p/79/passport.svg",
+        "https://avatars.githubusercontent.com/u/1529926?s=200&v=4",
+        "https://nodemailer.com/nm_logo_200x136.png",
+        "https://i.ibb.co/4SBhGSn/pngwing-com.png",
+        "https://socket.io/images/logo.svg",
+      ],
+
+    },
+    {
+      id: 1,
+      name: "Mi Blog",
+      thumbnail: "/images/thumbnails/MiBlog.png",
+      preview: "/images/Webp/eCommerce.webp",
+      url: "https://my-blog-1tqhl98hy-santi-iztli.vercel.app/",
+      tech: [
+        "/images/Icons/tech/node.webp",
+        "https://handlebarsjs.com/images/handlebars_logo.png",
+        "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
+        "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png?20141107110902",
+        "https://sass-lang.com/assets/img/logos/logo.svg",
+        "https://static.cdnlogo.com/logos/p/79/passport.svg",
+        "https://avatars.githubusercontent.com/u/1529926?s=200&v=4",
+      ],
+
+    },
+    {
+      id: 2,
+      name: "Abrazodelarbol",
+      thumbnail: "/images/thumbnails/www-abrazodelarbol-com.webp",
+      preview: "/images/Webp/AbrazoDelArbol.webp",
+      url: "https://abrazodelarbol.com",
+      tech: [
+        "/images/Icons/MercadoPago.png",
+        "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
+        "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png?20141107110902",
+        "https://sass-lang.com/assets/img/logos/logo.svg",
+
+      ],
+
+    },
+    {
+      id: "",
+      name: "",
+      thumbnail: "",
+      preview: "",
+      url: "",
+      tech: [
+
+      ],
+
+    },
+    {
+      id: "",
+      name: "",
+      thumbnail: "",
+      preview: "",
+      url: "",
+      tech: [
+
+      ],
+
+    },
+    {
+      id: "",
+      name: "",
+      thumbnail: "",
+      preview: "",
+      url: "",
+      tech: [
+
+      ],
+
+    },
+    {
+      id: "",
+      name: "",
+      thumbnail: "",
+      preview: "",
+      url: "",
+      tech: [
+
+      ],
+
+    },
+    {
+      id: "",
+      name: "",
+      thumbnail: "",
+      preview: "",
+      url: "",
+      tech: [
+
+      ],
+
+    },
+    {
+      id: "",
+      name: "",
+      thumbnail: "",
+      preview: "",
+      url: "",
+      tech: [
+
+      ],
+
+    },
+    {
+      id: "",
+      name: "",
+      thumbnail: "",
+      preview: "",
+      url: "",
+      tech: [
+
+      ],
+
+    },
+    {
+      id: "",
+      name: "",
+      thumbnail: "",
+      preview: "",
+      url: "",
+      tech: [
+
+      ],
+
+    },
+]
