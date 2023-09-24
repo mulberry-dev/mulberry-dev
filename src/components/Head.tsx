e10bdf30d01ae19bdac2c2682ecfa093fa12e3d8
SantiagoMorera <santi.iztli@gmail.com>
2023-09-24 16:15:25 -0600

    OG image

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,37 +1,37 @@
 const Head = () => {
   return (
     <>
-        {/* FavIcon */}
-        <link
-          rel="shortcut icon"
-          href="https://cdn-icons-png.flaticon.com/512/7914/7914802.png"
-          type="image"
-        />
-        <title>ThisIsSanti.dev | Web Programmer</title>
-        {/* Meta */}
-        <meta
-          name="description"
-          content="Programmer who loves code and technology, comitted to developing specialized and scalable technology in new projects."
-        />
-        <meta
-          name="keywords"
-          content="Developer, web, wordpress, app, ecommerce, SEO, ux,ui, css, JavaScript, Santiago, Morera, dev, Full Stack, Node.js, React.js, Next.js, Nest.js, TypeScript"
-        />
-        <meta name="copyright" content="© 2006 MDC"></meta>
-        {/* OpenGraph */}
-        <meta property="og:type" content="Portfolio" />
-        <meta property="og:title" content="Web Developer" />
-        <meta
-          property="og:description"
-          content="Full Stack Developer JavaScript/TypeScript"
-        />
-        <meta property="og:url" content="https://thisissanti.dev" />
-        <meta
-          property="og:image"
-          content="https://i.ibb.co/Fk1d2V3/santi-iztli-github-io-portfolio.png"
-        />
+      {/* FavIcon */}
+      <link
+        rel='shortcut icon'
+        href='https://cdn-icons-png.flaticon.com/512/7914/7914802.png'
+        type='image'
+      />
+      <title>ThisIsSanti.dev | Web Programmer</title>
+      {/* Meta */}
+      <meta
+        name='description'
+        content='Programmer who loves code and technology, comitted to developing specialized and scalable technology in new projects.'
+      />
+      <meta
+        name='keywords'
+        content='Developer, web, wordpress, app, ecommerce, SEO, ux,ui, css, JavaScript, Santiago, Morera, dev, Full Stack, Node.js, React.js, Next.js, Nest.js, TypeScript'
+      />
+      <meta name='copyright' content='© 2006 MDC'></meta>
+      {/* OpenGraph */}
+      <meta property='og:type' content='Portfolio' />
+      <meta property='og:title' content='Web Developer' />
+      <meta
+        property='og:description'
+        content='Full Stack Developer JavaScript/TypeScript'
+      />
+      <meta property='og:url' content='https://thisissanti.dev' />
+      <meta
+        property='og:image'
+        content='https://i.ibb.co/BwtSfMG/Captura-de-pantalla-2023-09-24-161329.png'
+      />
     </>
-  )
+  );
 }
 
 export default Head
