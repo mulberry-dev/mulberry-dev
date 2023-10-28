3893de37024695ede318d98867a6e307daf90fbf
SantiagoMorera <santi.iztli@gmail.com>
2023-10-28 13:01:08 -0600

    [Next 14 update]

--- src/utils/alerts.ts
+++ src/utils/alerts.ts
@@ -1,15 +1,15 @@
-import Swal from "sweetalert2";
+import Swal from "sweetalert2"
 
 export function alertInfo(params: string) {
   if (params) {
     Swal.fire({
       icon: "info",
       title: "App developed with: <br/> <span>Next.js<span/>",
-      text: "Version: 13.5",
+      text: "Version: 14",
       focusConfirm: false,
       confirmButtonText: "Great!",
       confirmButtonAriaLabel: "Thumbs up, great!",
       confirmButtonColor: "#3fc3ee",
-    });
+    })
   }
 }
