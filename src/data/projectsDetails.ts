e5ae6397da8329d6e356957628bc4c3fe8ca6873
SantiagoMorera <santi.iztli@gmail.com>
2023-12-05 19:49:01 -0600

    [update]

--- src/data/projectsDetails.ts
+++ src/data/projectsDetails.ts
@@ -1,114 +1,114 @@
 export const projectsDetails = [
-    {
-      id: 0,
-      name: "eCommerce",
-      img: "/images/Webp/eCommerce.webp",
-      description:
-        "eCommerce that contains a chat, developed in Node.js, it allows you to add products to the shopping cart as well as delete them, it has a login and authentication, when you register you will receive an email, you can access your profile and see your data, if you complete a purchase, you will receive an email with the purchased products. Testing with Mocha. (2023)",
-      tech: [
-        "/images/Icons/tech/node.webp",
-        "https://handlebarsjs.com/images/handlebars_logo.png",
-        "/images/Icons/tech/mocha.svg",
-        "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
-        "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png?20141107110902",
-        "https://www.pngall.com/wp-content/uploads/13/Mongodb-PNG-Image-HD.png",
-        "https://sass-lang.com/assets/img/logos/logo.svg",
-        "https://static.cdnlogo.com/logos/p/79/passport.svg",
-        "https://avatars.githubusercontent.com/u/1529926?s=200&v=4",
-        "https://nodemailer.com/nm_logo_200x136.png",
-        "https://i.ibb.co/4SBhGSn/pngwing-com.png",
-        "https://socket.io/images/logo.svg",
-      ],
-      url: "https://ecommerce-backend-43495.fly.dev/",
-    },
-    {
-      id: 1,
-      name: "Pokedex",
-      img: "/images/Webp/Pokedex.webp",
-      description:
-        "This Node.js application allows you to see a list of 1000 Pokemons and add your favorite pokemons to a list as well as eliminate them, you can search for any Pokemon and see all its characteristics, it has login and authentication. (2023)",
-      tech: [
-        "/images/Icons/tech/node.webp",
-        "https://handlebarsjs.com/images/handlebars_logo.png",
-        "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
-        "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png?20141107110902",
-        "https://sass-lang.com/assets/img/logos/logo.svg",
-        "https://static.cdnlogo.com/logos/p/79/passport.svg",
-        "https://avatars.githubusercontent.com/u/1529926?s=200&v=4",
-      ],
-      url: "https://pokedex-santi-dev.fly.dev/",
-    },
-    {
-      id: 2,
-      name: "Abrazo del Arbol",
-      description:
-        "Landing page developed in Vanilla JavaScript, for a martial arts school, it allows you to make purchases of courses and products through Mercado Pago. (2023)",
-      img: "/images/Webp/AbrazoDelArbol.webp",
-      tech: [
-        "/images/Icons/MercadoPago.png",
-        "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
-        "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png?20141107110902",
-        "https://sass-lang.com/assets/img/logos/logo.svg",
-      ],
-      url: "https://abrazodelarbol.com",
-    },
-    {
-      id: 3,
-      name: "Live Chat App",
-      img: "/images/Webp/LiveChatApp.webp",
-      description:
-        "Select a chat room, chat with the people who are inside the room, it will show you the time of the messages, when you decide you can leave the room and return to the home page, developed in Node.js. (2023)",
-      tech: [
-        "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
-        "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
-        "https://socket.io/images/logo.svg",
-        "/images/Icons/tech/node.webp",
-      ],
-      url: "https://live-chat-app3.glitch.me/",
-    },
-    {
-      id: 4,
-      name: "Tecno Shop",
-      description:
-        "This application is a computer ecommerce developed in React.js, where you can choose a category of products, add them to a shopping cart as well as delete them, at the end of the purchase, the stock of the available product decreases.",
-      img: "/images/Webp/TecnoShop.webp",
-      tech: [
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/250px-React.svg.png",
-        "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
-        "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
-        "https://sass-lang.com/assets/img/logos/logo.svg",
-        "https://i.ibb.co/LCqHmyX/pngwing-com-1.png",
-      ],
-      url: "https://tecno-tienda.netlify.app/",
-    },
-    {
-      id: 5,
-      name: "Naturatours",
-      description:
-        "This website was developed with the CMS WordPress, for a tour agency in Puerto Vallarta, a landing page that allows you to publicize your products as well as make reservations and close sales. (2020)",
-      img: "https://i.ibb.co/VTrr8C7/Natura-Tours.webp",
-      tech: ["https://cdn-icons-png.flaticon.com/512/536/536493.png"],
-      url: "https://naturatours.com.mx/",
-    },
-    {
-      id: 6,
-      name: "eCommerce Js",
-      img: "/images/Webp/eCommerceJs.webp",
-      description:
-        "This is a web page developed with Vanilla JavaScript, it is an eCommerce that allows you to add the products to the cart, as well as finalize the purchase, it has a dark/light theme, it was the final project of a JavaScript course. (2022)",
-      tech: [
-        "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
-        "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png?20141107110902",
-        "https://www.pngall.com/wp-content/uploads/13/Mongodb-PNG-Image-HD.png",
-        "https://sass-lang.com/assets/img/logos/logo.svg",
-      ],
-      url: "https://santiagomorera.github.io/proyectoJS/#index",
-    },
-    /* {
+  {
+    id: 0,
+    name: "eCommerce",
+    img: "/images/Webp/eCommerce.webp",
+    description:
+      "eCommerce that contains a chat, developed in Node.js, it allows you to add products to the shopping cart as well as delete them, it has a login and authentication, when you register you will receive an email, you can access your profile and see your data, if you complete a purchase, you will receive an email with the purchased products. Testing with Mocha. (2023)",
+    tech: [
+      "/images/Icons/tech/node.webp",
+      "https://handlebarsjs.com/images/handlebars_logo.png",
+      "/images/Icons/tech/mocha.svg",
+      "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
+      "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png?20141107110902",
+      "https://www.pngall.com/wp-content/uploads/13/Mongodb-PNG-Image-HD.png",
+      "https://sass-lang.com/assets/img/logos/logo.svg",
+      "https://static.cdnlogo.com/logos/p/79/passport.svg",
+      "https://avatars.githubusercontent.com/u/1529926?s=200&v=4",
+      "https://nodemailer.com/nm_logo_200x136.png",
+      "https://i.ibb.co/4SBhGSn/pngwing-com.png",
+      "https://socket.io/images/logo.svg",
+    ],
+    url: "https://ecommerce-backend-43495.fly.dev/",
+  },
+  {
+    id: 1,
+    name: "Pokedex",
+    img: "/images/Webp/Pokedex.webp",
+    description:
+      "This Node.js application allows you to see a list of 1000 Pokemons and add your favorite pokemons to a list as well as eliminate them, you can search for any Pokemon and see all its characteristics, it has login and authentication. (2023)",
+    tech: [
+      "/images/Icons/tech/node.webp",
+      "https://handlebarsjs.com/images/handlebars_logo.png",
+      "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
+      "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png?20141107110902",
+      "https://sass-lang.com/assets/img/logos/logo.svg",
+      "https://static.cdnlogo.com/logos/p/79/passport.svg",
+      "https://avatars.githubusercontent.com/u/1529926?s=200&v=4",
+    ],
+    url: "https://pokedex-santi-dev.fly.dev/",
+  },
+  {
+    id: 2,
+    name: "Abrazo del Arbol",
+    description:
+      "Landing page developed in Vanilla JavaScript, for a martial arts school, it allows you to make purchases of courses and products through Mercado Pago. (2023)",
+    img: "/images/Webp/AbrazoDelArbol.webp",
+    tech: [
+      "/images/Icons/MercadoPago.png",
+      "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
+      "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png?20141107110902",
+      "https://sass-lang.com/assets/img/logos/logo.svg",
+    ],
+    url: null,
+  },
+  {
+    id: 3,
+    name: "Live Chat App",
+    img: "/images/Webp/LiveChatApp.webp",
+    description:
+      "Select a chat room, chat with the people who are inside the room, it will show you the time of the messages, when you decide you can leave the room and return to the home page, developed in Node.js. (2023)",
+    tech: [
+      "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
+      "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
+      "https://socket.io/images/logo.svg",
+      "/images/Icons/tech/node.webp",
+    ],
+    url: "https://live-chat-app3.glitch.me/",
+  },
+  {
+    id: 4,
+    name: "Tecno Shop",
+    description:
+      "This application is a computer ecommerce developed in React.js, where you can choose a category of products, add them to a shopping cart as well as delete them, at the end of the purchase, the stock of the available product decreases.",
+    img: "/images/Webp/TecnoShop.webp",
+    tech: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/250px-React.svg.png",
+      "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
+      "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
+      "https://sass-lang.com/assets/img/logos/logo.svg",
+      "https://i.ibb.co/LCqHmyX/pngwing-com-1.png",
+    ],
+    url: "https://tecno-tienda.netlify.app/",
+  },
+  {
+    id: 5,
+    name: "Naturatours",
+    description:
+      "This website was developed with the CMS WordPress, for a tour agency in Puerto Vallarta, a landing page that allows you to publicize your products as well as make reservations and close sales. (2020)",
+    img: "https://i.ibb.co/VTrr8C7/Natura-Tours.webp",
+    tech: ["https://cdn-icons-png.flaticon.com/512/536/536493.png"],
+    url: "https://naturatours.com.mx/",
+  },
+  {
+    id: 6,
+    name: "eCommerce Js",
+    img: "/images/Webp/eCommerceJs.webp",
+    description:
+      "This is a web page developed with Vanilla JavaScript, it is an eCommerce that allows you to add the products to the cart, as well as finalize the purchase, it has a dark/light theme, it was the final project of a JavaScript course. (2022)",
+    tech: [
+      "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
+      "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png?20141107110902",
+      "https://www.pngall.com/wp-content/uploads/13/Mongodb-PNG-Image-HD.png",
+      "https://sass-lang.com/assets/img/logos/logo.svg",
+    ],
+    url: null,
+  },
+  /* {
       id: 7,
       name: "Invitation",
       description:
@@ -122,19 +122,20 @@ export const projectsDetails = [
       ],
       url: "https://santiagomorera.github.io/DeliaYSantiagoInvi/",
     }, */
-    {
-      id: 8,
-      name: "Mi Blog",
-      description: "Blog developed in Next.js, with admin panel where you can add or delete your posts, it has a login and authentication. Offline Mode with Local Storage. (2023)",
-      img: "/images/thumbnails/MiBlog.png",
+  {
+    id: 8,
+    name: "Mi Blog",
+    description:
+      "Blog developed in Next.js, with admin panel where you can add or delete your posts, it has a login and authentication. Offline Mode with Local Storage. (2023)",
+    img: "/images/thumbnails/MiBlog.png",
 
-      tech: [
-        "/images/Icons/tech/NextJs.webp",
-        "/images/Icons/mongodb-icon-1.svg"
-      ],
-      url: null,
-    },
-    /* {
+    tech: [
+      "/images/Icons/tech/NextJs.webp",
+      "/images/Icons/mongodb-icon-1.svg",
+    ],
+    url: null,
+  },
+  /* {
       id: 8,
       name: "Account Statement Generator",
       description: "Account Statement Generator",
@@ -147,4 +148,4 @@ export const projectsDetails = [
       ],
       url: "#",
     }, */
-  ];
+]
