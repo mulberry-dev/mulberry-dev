dd80ab3018db647e1e63786b78b09fa2847d0037
Santiago Morera <96962537+SantiagoMorera@users.noreply.github.com>
2023-12-15 20:54:37 -0600

    [update]

--- src/utils/alerts.ts
+++ src/utils/alerts.ts
@@ -1,11 +1,13 @@
 import Swal from "sweetalert2"
 
+const lastUpdate = "Dicember 15th 2023"
+
 export function alertInfo(params: string) {
   if (params) {
     Swal.fire({
       icon: "info",
       title: "App developed with: <br/> <span>Next.js<span/>",
-      text: "Last update: Dicember 15th 2023",
+      text: `Last update: ${lastUpdate}`,
       focusConfirm: false,
       confirmButtonText: "Great!",
       confirmButtonAriaLabel: "Thumbs up, great!",
