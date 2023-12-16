f71013442e0c3073c917d110934cf6a8e77e097e
Santiago Morera <96962537+SantiagoMorera@users.noreply.github.com>
2023-12-15 20:46:45 -0600

    [update]

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,34 +1,34 @@
 export const projectsPortfolio = [
-    {
-      id: 0,
-      name: "eCommerce",
-      img: "/images/thumbnails/Thumbnail-e-Commerce-Backend.webp",
-      tech: ["Node.js", "React.js", "HTML", "CSS3"],
-      url: "https://ecommerce-backend-43495.fly.dev/",
-    },
-    {
-      id: 8,
-      name: "Mi Blog",
-      description: "Blog with admin panel.",
-      img: "/images/thumbnails/MiBlog.png",
-      url: null,
-    },
+  {
+    id: 0,
+    name: "eCommerce",
+    img: "/images/thumbnails/Thumbnail-e-Commerce-Backend.webp",
+    tech: ["Node.js", "React.js", "HTML", "CSS3"],
+    url: "https://ecommerce-backend-43495.fly.dev/",
+  },
+  {
+    id: 8,
+    name: "Mi Blog",
+    description: "Blog with admin panel.",
+    img: "/images/thumbnails/MiBlog.png",
+    url: null,
+  },
 
-    {
-      id: 2,
-      name: "Abrazodelarbol",
-      img: "/images/thumbnails/www-abrazodelarbol-com.webp",
-      tech: ["Node.js", "React.js", "HTML", "CSS3"],
-      url: "https://abrazodelarbol.com",
-    },
-    {
-      id: 5,
-      name: "Naturatours",
-      img: "/images/thumbnails/Thumbnail-naturatours.webp",
-      tech: ["WordPress"],
-      url: "https://naturatours.com.mx/",
-    },
-    /* {
+  {
+    id: 2,
+    name: "Abrazodelarbol",
+    img: "/images/thumbnails/www-abrazodelarbol-com.webp",
+    tech: ["Node.js", "React.js", "HTML", "CSS3"],
+    url: "https://abrazodelarbol.com",
+  },
+  {
+    id: 5,
+    name: "Naturatours",
+    img: "/images/thumbnails/Thumbnail-naturatours.webp",
+    tech: ["WordPress"],
+    url: "https://naturatours.com.mx/",
+  },
+  /* {
       id: 7,
       name: "Invitation",
       description: "Wedding invitation with google form.",
@@ -40,45 +40,45 @@ export const projectsPortfolio = [
       ],
       url: "https://santiagomorera.github.io/proyectoJS/#index",
     }, */
-    {
-      id: 3,
-      name: "Live Chat App",
-      img: "/images/thumbnails/Thumbnail-Chat-App.webp",
-      tech: ["Node.js", "React.js", "HTML", "CSS3"],
-      url: "https://live-chat-app3.glitch.me/",
-    },
-    {
-      id: 4,
-      name: "Tecno Shop",
-      img: "/images/thumbnails/tecnotienda.webp",
-      tech: ["Node.js", "React.js", "HTML", "CSS3"],
-      url: "https://tecno-tienda.netlify.app/",
-    },
-    {
-      id: 6,
-      name: "eCommerce Js",
-      img: "/images/thumbnails/Thumbnail-e-Commerce-JS.webp",
-      tech: ["Node.js", "React.js", "HTML", "CSS3"],
-      url: "https://santiagomorera.github.io/proyectoJS/#index",
-    },
-    {
-      id: 1,
-      name: "Pokedex",
-      img: "/images/thumbnails/Thumbnail-Screenshot-Pokedex.webp",
-      tech: ["Node.js", "React.js", "HTML", "CSS3"],
-      url: "https://pokedex-santi-dev.fly.dev/",
-    },
+  {
+    id: 3,
+    name: "Live Chat App",
+    img: "/images/thumbnails/Thumbnail-Chat-App.webp",
+    tech: ["Node.js", "React.js", "HTML", "CSS3"],
+    url: "https://live-chat-app3.glitch.me/",
+  },
+  {
+    id: 4,
+    name: "Tecno Shop",
+    img: "/images/thumbnails/tecnotienda.webp",
+    tech: ["Node.js", "React.js", "HTML", "CSS3"],
+    url: "https://tecno-tienda.netlify.app/",
+  },
+  {
+    id: 6,
+    name: "eCommerce Js",
+    img: "/images/thumbnails/Thumbnail-e-Commerce-JS.webp",
+    tech: ["Node.js", "React.js", "HTML", "CSS3"],
+    url: "https://santiagomorera.github.io/proyectoJS/#index",
+  },
+  {
+    id: 1,
+    name: "Pokedex",
+    img: "/images/thumbnails/Thumbnail-Screenshot-Pokedex.webp",
+    tech: ["Node.js", "React.js", "HTML", "CSS3"],
+    url: "https://pokedex-santi-dev.fly.dev/",
+  },
 
-    /* {
-      id: 8,
-      name: "Statement Generator",
-      description: "Account Statement Generator",
-      img: "https://i.ibb.co/jDS8QD4/Captura-de-pantalla-2023-08-21-163247.png",
-      tech: [
-        "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
-        "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
-        "https://www.freepnglogos.com/uploads/javascript-png/png-javascript-badge-picture-8.png",
-      ],
-      url: "#",
-    }, */
-  ];
+  {
+    id: 8,
+    name: "Statement Generator",
+    description: "Account Statement Generator",
+    img: "https://i.ibb.co/jDS8QD4/Captura-de-pantalla-2023-08-21-163247.png",
+    tech: [
+      "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
+      "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
+      "https://www.freepnglogos.com/uploads/javascript-png/png-javascript-badge-picture-8.png",
+    ],
+    url: "#",
+  },
+]
