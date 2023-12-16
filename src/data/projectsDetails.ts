94ba7a27d12c951456f2fe69265d0656a032476d
Santiago Morera <96962537+SantiagoMorera@users.noreply.github.com>
2023-12-15 20:43:03 -0600

    [update]

--- src/data/projectsDetails.ts
+++ src/data/projectsDetails.ts
@@ -4,7 +4,7 @@ export const projectsDetails = [
     name: "eCommerce",
     img: "/images/Webp/eCommerce.webp",
     description:
-      "eCommerce that contains a chat, developed in Node.js, it allows you to add products to the shopping cart as well as delete them, it has a login and authentication, when you register you will receive an email, you can access your profile and see your data, if you complete a purchase, you will receive an email with the purchased products. Testing with Mocha. (2023)",
+      "An eCommerce platform, developed in Node.js, features a chat functionality. It enables users to add and delete products from the shopping cart, incorporates a login and authentication system, and sends a confirmation email upon registration. Users can access their profiles, view personal data, and upon completing a purchase, receive an email containing details of the purchased products. The platform is tested using Mocha. (2023)",
     tech: [
       "/images/Icons/tech/node.webp",
       "https://handlebarsjs.com/images/handlebars_logo.png",
@@ -27,7 +27,7 @@ export const projectsDetails = [
     name: "Pokedex",
     img: "/images/Webp/Pokedex.webp",
     description:
-      "This Node.js application allows you to see a list of 1000 Pokemons and add your favorite pokemons to a list as well as eliminate them, you can search for any Pokemon and see all its characteristics, it has login and authentication. (2023)",
+      "This Node.js application enables users to view a list of 1000 Pokémon, add their favorite Pokémon to a list, and remove them. Users can search for any Pokémon and access all its characteristics. The application includes a login and authentication feature. (2023)",
     tech: [
       "/images/Icons/tech/node.webp",
       "https://handlebarsjs.com/images/handlebars_logo.png",
@@ -44,7 +44,7 @@ export const projectsDetails = [
     id: 2,
     name: "Abrazo del Arbol",
     description:
-      "Landing page developed in Vanilla JavaScript, for a martial arts school, it allows you to make purchases of courses and products through Mercado Pago. (2023)",
+      "A landing page developed in Vanilla JavaScript for a martial arts school. It facilitates course and product purchases through Mercado Pago. (2023)",
     img: "/images/Webp/AbrazoDelArbol.webp",
     tech: [
       "/images/Icons/MercadoPago.png",
@@ -60,7 +60,7 @@ export const projectsDetails = [
     name: "Live Chat App",
     img: "/images/Webp/LiveChatApp.webp",
     description:
-      "Select a chat room, chat with the people who are inside the room, it will show you the time of the messages, when you decide you can leave the room and return to the home page, developed in Node.js. (2023)",
+      "Choose a chat room, engage in conversations with participants inside, and view message timestamps. When ready, you can exit the room and return to the home page. Developed in Node.js. (2023)",
     tech: [
       "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
       "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
@@ -73,8 +73,7 @@ export const projectsDetails = [
     id: 4,
     name: "Tecno Shop",
     description:
-      "This application is a computer ecommerce developed in React.js, where you can choose a category of products, add them to a shopping cart as well as delete them, at the end of the purchase, the stock of the available product decreases.",
-    img: "/images/Webp/TecnoShop.webp",
+"This application is a computer e-commerce platform developed in React.js. Users can select a category of products, add items to a shopping cart, and remove them. Upon completing a purchase, the available product stock decreases.",    img: "/images/Webp/TecnoShop.webp",
     tech: [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/250px-React.svg.png",
       "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
@@ -88,8 +87,7 @@ export const projectsDetails = [
     id: 5,
     name: "Naturatours",
     description:
-      "This website was developed with the CMS WordPress, for a tour agency in Puerto Vallarta, a landing page that allows you to publicize your products as well as make reservations and close sales. (2020)",
-    img: "https://i.ibb.co/VTrr8C7/Natura-Tours.webp",
+"This website was developed using the CMS WordPress for a tour agency in Puerto Vallarta. It features a landing page that allows you to showcase products, make reservations, and complete sales. (2020)",    img: "https://i.ibb.co/VTrr8C7/Natura-Tours.webp",
     tech: ["https://cdn-icons-png.flaticon.com/512/536/536493.png"],
     url: "https://naturatours.com.mx/",
   },
@@ -98,8 +96,7 @@ export const projectsDetails = [
     name: "eCommerce Js",
     img: "/images/Webp/eCommerceJs.webp",
     description:
-      "This is a web page developed with Vanilla JavaScript, it is an eCommerce that allows you to add the products to the cart, as well as finalize the purchase, it has a dark/light theme, it was the final project of a JavaScript course. (2022)",
-    tech: [
+"This web page, developed with Vanilla JavaScript, is an eCommerce platform that enables users to add products to the cart and complete purchases. It includes a dark/light theme and served as the final project for a JavaScript course. (2022)",    tech: [
       "https://cdn-icons-png.flaticon.com/512/5968/5968267.png",
       "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
       "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png?20141107110902",
@@ -126,7 +123,7 @@ export const projectsDetails = [
     id: 8,
     name: "Mi Blog",
     description:
-      "Blog developed in Next.js, with admin panel where you can add or delete your posts, it has a login and authentication. Offline Mode with Local Storage. (2023)",
+      "A blog developed in Next.js includes an admin panel that allows users to add or delete posts. The platform features a login and authentication system and supports an Offline Mode with Local Storage. (2023)",
     img: "/images/thumbnails/MiBlog.png",
 
     tech: [
