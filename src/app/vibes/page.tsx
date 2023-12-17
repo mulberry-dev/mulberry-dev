08de2077429b11d6278c16757bf138323cb02afa
Santiago Morera <96962537+SantiagoMorera@users.noreply.github.com>
2023-12-16 21:50:56 -0600

    [update]

--- src/app/vibes/page.tsx
+++ src/app/vibes/page.tsx
@@ -5,16 +5,16 @@ const page = () => {
     <section>
       <h2>Vibes Test</h2>
       <div className='vibration-container'>
-        <div onClick={() => navigator.vibrate(100)}>Vibrate 100</div>
-        <div onClick={() => navigator.vibrate(200)}>Vibrate 200</div>
-        <div onClick={() => navigator.vibrate(300)}>Vibrate 300</div>
-        <div onClick={() => navigator.vibrate(400)}>Vibrate 400</div>
-        <div onClick={() => navigator.vibrate(500)}>Vibrate 500</div>
-        <div onClick={() => navigator.vibrate(600)}>Vibrate 600</div>
-        <div onClick={() => navigator.vibrate(700)}>Vibrate 700</div>
-        <div onClick={() => navigator.vibrate(800)}>Vibrate 800</div>
-        <div onClick={() => navigator.vibrate(900)}>Vibrate 900</div>
-        <div onClick={() => navigator.vibrate(1000)}>Vibrate 1000</div>
+        <div onClick={() => window.navigator.vibrate(100)}>Vibrate 100</div>
+        <div onClick={() => window.navigator.vibrate(200)}>Vibrate 200</div>
+        <div onClick={() => window.navigator.vibrate(300)}>Vibrate 300</div>
+        <div onClick={() => window.navigator.vibrate(400)}>Vibrate 400</div>
+        <div onClick={() => window.navigator.vibrate(500)}>Vibrate 500</div>
+        <div onClick={() => window.navigator.vibrate(600)}>Vibrate 600</div>
+        <div onClick={() => window.navigator.vibrate(700)}>Vibrate 700</div>
+        <div onClick={() => window.navigator.vibrate(800)}>Vibrate 800</div>
+        <div onClick={() => window.navigator.vibrate(900)}>Vibrate 900</div>
+        <div onClick={() => window.navigator.vibrate(1000)}>Vibrate 1000</div>
       </div>
     </section>
   )
