f0d9b16029e39e3457d713ccaddb4734c5c8a570
Santiago Morera <96962537+SantiagoMorera@users.noreply.github.com>
2023-12-19 23:49:26 -0600

    [update]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,28 @@
-"use client";
+"use client"
 
-import Link from "next/link";
-import { useEffect } from "react";
+import Link from "next/link"
+import { useEffect } from "react"
 
-const IndexPage =  () => {
-
+const IndexPage = () => {
   useEffect(() => {
-    document.title = 'ThisIsSanti.dev | Home';
-  }, []);
+    document.title = "ThisIsSanti.dev | Home"
+  }, [])
   return (
     <>
-      <section id="index">
-        <div className="mainPage-container animate__animated animate__fadeIn">
-          <h1 className="color_title rainbow_bg"> Hi! I am Santiago</h1>
-          <p className="full_stack_title">
+      <section id='index'>
+        <div className='mainPage-container animate__animated animate__fadeIn'>
+          <h1 className='color_title rainbow_bg'> Hi! I am Santiago</h1>
+          <p className='full_stack_title'>
             <span> &lt; </span>Full Stack Developer<span> /&gt; </span>
           </p>
-          <p className="javascript_title">Java Script</p>
-          <Link className="button-generic button_animation" href="/about">
+          <p className='javascript_title'>Java Script</p>
+          <Link className='button-generic button_animation' href='/about'>
             About
           </Link>
         </div>
       </section>
     </>
-  );
-};
+  )
+}
 
-export default IndexPage;
+export default IndexPage
