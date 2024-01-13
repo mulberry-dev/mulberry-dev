e5c77ae8c2122cdd8cc29e07c65622cfb688ab1a
Santiago Morera <96962537+SantiagoMorera@users.noreply.github.com>
2024-01-13 14:34:16 -0600

    [update]

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -157,7 +157,7 @@ export const data = [
       "https://static.cdnlogo.com/logos/p/79/passport.svg",
       "https://avatars.githubusercontent.com/u/1529926?s=200&v=4",
     ],
-    url: "https://pokedex-santi-dev.fly.dev",
+    url: null,
     thumbnail: "/images/thumbnails/Thumbnail-Screenshot-Pokedex.webp",
   },
 ]
