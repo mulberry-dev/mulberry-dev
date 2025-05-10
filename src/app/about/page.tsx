a10bb0bb86e25e47acde21b3faac7bbc5dd40008
SantiagoMorera <santi.iztli@gmail.com>
2025-05-10 15:50:40 -0600

    About photo updated

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,59 +4,61 @@ import Image from "next/image"
 import Link from "next/link"
 import { useEffect } from "react"
 
+const AvatarImage = ({ src, className }: { src: string; className: string }) =>
+  <Image
+    src={src}
+    alt="Santi Avatar"
+    priority={true}
+    width={250}
+    height={250}
+    quality={100}
+    title="Santi Avatar"
+    className={`${className} transition-opacity opacity-0 duration-2s`}
+    onLoad={image => image.currentTarget.classList.remove("opacity-0")}
+  />
+
 const About = () => {
   useEffect(() => {
     document.title = "About | ThisIsSanti.dev"
-
-    document.body.classList.add('about');
-  
-    return () => {
-      document.body.classList.remove('about');
-    };
+    document.body.classList.add("about")
+    return () => document.body.classList.remove("about")
   }, [])
+
   return (
-    <>
-      <section id='about'>
-        <div className='about_wrapper square-animation animate__animated'>
-          <Image
-            src='/images/Icons/avatar-santi-git.svg'
-            //src='/images/Icons/AvatarSanti.webp'  
-            alt='Santi Avatar'
-            priority={true}
-            width={250}
-            height={250}
-            quality={100}
-            title='Santi Avatar'
-            className='santi_avatar transition-opacity opacity-0 duration-2s'
-            onLoad={(image) => image.currentTarget.classList.remove("opacity-0")}
-          />
-          {
-            <p className='about_text animate__fadeIn'>
-              I am a programmer who has a deep love for coding and technology. My
-              passion extends to UX & UI design, I consider myself an expert
-              in Frontend and a devoted enthusiast of Backend. I am always
-              immersed in learning new technologies, driven by my commitment to
-              developing specialized and scalable technology for new projects.
-            </p>
-          }
-          <div className='buttons_container'>
-            <Link className='button smallest ' href={"/"}>
-              <Image
-                src='/images/Icons/arrow-right-rounded.svg'
-                alt='go-back'
-                className='go_back'
-                width={40}
-                height={40}
-                title='Go back'
-              />
-            </Link>
-            <Link className='button-generic' href='/skills'>
-              Skills
-            </Link>
-          </div>
+    <section id="about">
+      <div className="about_wrapper square-animation animate__animated">
+        <AvatarImage
+          src="/images/Webp/santi-light-theme.webp"
+          className="santi_avatar_light_theme"
+        />
+        <AvatarImage
+          src="/images/Webp/santi-dark-theme.webp"
+          className="santi_avatar_dark_theme"
+        />
+        <p className="about_text animate__fadeIn">
+          I am a programmer who has a deep love for coding and technology. My
+          passion extends to UX & UI design, I consider myself an expert in
+          Frontend and a devoted enthusiast of Backend. I am always immersed in
+          learning new technologies, driven by my commitment to developing
+          specialized and scalable technology for new projects.
+        </p>
+        <div className="buttons_container">
+          <Link className="button smallest" href={"/"}>
+            <Image
+              src="/images/Icons/arrow-right-rounded.svg"
+              alt="go-back"
+              className="go_back"
+              width={40}
+              height={40}
+              title="Go back"
+            />
+          </Link>
+          <Link className="button-generic" href="/skills">
+            Skills
+          </Link>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   )
 }
 
--- src/utils/alerts.ts
+++ src/utils/alerts.ts
@@ -1,6 +1,6 @@
 import Swal from "sweetalert2"
 
-const lastUpdate = "February 17 2025"
+const lastUpdate = "May 10 2025"
 
 
 export function alertInfo(params: string) {
